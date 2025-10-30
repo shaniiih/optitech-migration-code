a3015e78b4c3fe1fad0751ee123f3a738e063e32
Akshay Mahajan <akshay.iihglobal@gmail.com>
2025-10-30 13:30:26 +0530

    set verified flag

--- main.js
+++ main.js
@@ -165,7 +165,7 @@ async function ensureTenant(tenantId) {
     await runStep("LowVisionArea", () => migrateLowVisionArea(tenantId)); // Verified
     await runStep("GlassColor", () => migrateGlassColor(tenantId)); // Verified
     await runStep("GlassRole", () => migrateGlassRole(tenantId)); // Verified
-    await runStep("MovementType", () => migrateMovementType(tenantId));
+    await runStep("MovementType", () => migrateMovementType(tenantId)); // Verified
     await runStep("MovementProperty", () => migrateMovementProperty(tenantId)); // Verified
     await runStep("GlassCoating", () => migrateGlassCoating(tenantId)); // Verified
     await runStep("GlassMaterial", () => migrateGlassMaterial(tenantId)); // Verified
